466b1dd1357cf3a224765c9c207f08c5b25d7e6e
Yonathan <yonidoe15@gmail.com>
2019-03-28 16:28:00 -0400

    updated make command to work with docker toolbox

--- op/op-assert.sh
+++ op/op-assert.sh
@@ -9,7 +9,7 @@ else
     mkdir /var/log/nginx/
   fi && \
   # FIXME commands other than gen-certs should be run as non-root user
-  make gen-certs env composer-install laravel-init fresh-db fake-data npm-install-cross-env-global npm-install npm-dev && \
+  make gen-certs env composer-install laravel-init fresh-db fake-data npm-install-cross-env-global npm-install-no-bin-links npm-dev && \
   for i in storage bootstrap/cache; do
     chgrp -R www-data $BASE/$i && \
     chmod -R g+rxws $BASE/$i
